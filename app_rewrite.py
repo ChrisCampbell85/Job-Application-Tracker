c4bb2ec94d7aa2895271de7ebc2e1ec7640d5dd0
Chris Campbell <chris.campbell1@live.com>
2023-10-10 19:19:10 -0700

    Built out Show class
    Added logic to Show and changed some config so it all  fits in the frame

--- app_rewrite.py
+++ app_rewrite.py
@@ -92,15 +92,58 @@ class Show:
         self.frame = Frame(root)
         self.frame.pack()
         self.main_menu_frame = main_menu_frame
-        ttk.Label(self.frame, text='Show ME', font='30').pack(fill='y', pady=10)
-        self.back_button = utility_buttons(self.frame, self.main_menu_frame).back_button()
-        self.quit_button = utility_buttons(self.frame, self.main_menu_frame).quit_button()
-
+        self.utility_buttons = utility_buttons
+        table = read_data_from_db()
+        sorted_table = self.sort_db_table(table)
+        message, applications = self.create_message(sorted_table)
+        self.create_display(message, applications)
+        self.utility_buttons(self.frame, self.main_menu_frame).back_button()
+        self.utility_buttons(self.frame, self.main_menu_frame).quit_button()
+        
     def __str__(self):
         return "Show All"
+
+    def sort_db_table(self, table): 
+        """Matches each entry label with entry data. Returns list containing dict of database records"""
+        sorted_database_list = []
+        for item in table:
+            entry = dict((zip(DB_COLUMNS, item)))
+            sorted_database_list.append(entry)
+        
+        return sorted_database_list
+
+    def create_message(self, sorted_table):
+        """Creates string to view in Text widget"""
+        message = ''
+        for application in sorted_table:
+            message += '\n'
+            for label, entry in application.items():
+                label = format_string(label)
+                entry = format_string(entry, column=False)
+                text_label = f'{label}: {entry}\n'
+                message += text_label
+        applications = len(sorted_table)
+
+        return (message, applications)
+
+    def create_display(self, message, applications):
+        """Insert message into Text widget, add a scrollbar"""
+        position = 'position'
+        if applications > 1:
+            position = position + 's'
+        else:
+            position
+        label_text = f'You have applied for {applications} {position}'
+        ttk.Label(master=self.frame, text=label_text, font='8').pack()
+        message_display = Text(master=self.frame, font='7', width=50)
+        message_display.insert(END, message)
+        message_scroll = ttk.Scrollbar(master=self.frame, command=message_display.yview)
+        message_display.config(state='disabled', yscrollcommand=message_scroll.set)
+        message_display.pack(side=LEFT)
+        message_scroll.pack(side=RIGHT, fill=Y)
         
 class utilityButtons:
-    def __init__(self, frame, main_menu_frame) -> None:
+    def __init__(self, frame, main_menu_frame=None) -> None:
         self.frame = frame
         self.main_menu_frame = main_menu_frame
         
@@ -116,27 +159,28 @@ class utilityButtons:
     
 
 class App:
-    def __init__(self):
+    def __init__(self, utility_buttons):
         self.root = Tk()
         self.configureRoot()
+        self.utility_buttons = utility_buttons
         self.frame = Frame(master=self.root)
-        self.frame.pack()
+        self.frame.pack(expand=TRUE)
         self.homeButtons()
+        self.utility_buttons(self.frame).quit_button()
         self.root.mainloop()
 
     def configureRoot(self):
         self.root.title('Application Tracker')
-        self.root.geometry('400x580')
+        self.root.geometry('700x620')
 
     def homeButtons(self):
         style_tbutton()
         createButton = ttk.Button(self.frame, text=Create.__str__(self), command=lambda: self.changeFrame(Create)).pack(fill='both', padx=1, pady=10)
         showButton = ttk.Button(self.frame, text=Show.__str__(self), command=lambda: self.changeFrame(Show)).pack(fill='both', padx=1, pady=10)
          
-
     def changeFrame(self, cls):
         self.frame.pack_forget()
         cls(self.root, self.frame, utilityButtons)
         
 
-app = App()
+app = App(utilityButtons)
